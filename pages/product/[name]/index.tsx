0eab191de354ad6deac7d257f3aee949c862c38a
karam <karamkaku2000@gmail.com>
2022-10-04 21:35:37 -0400

    style: style description section

--- pages/product/[name]/index.tsx
+++ pages/product/[name]/index.tsx
@@ -1,3 +1,4 @@
+import * as React   from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {useRouter} from "next/router";
@@ -11,6 +12,12 @@ import discover from   "../../../asset/images/svg/discover.svg";
 const Index:NextPage = ()=>{
     const router = useRouter()
     const {name} = router.query;
+    const [sectionType,setSectionType] = React.useState("description")
+    const handleChangeSection = (sectionType:string)=>{
+        return ()=>{
+            setSectionType(sectionType)
+        }
+    }
     return(
          <section className="container container-primary-px bg-lightGrey py-[3rem] mb-[3rem]" tabIndex={-1}>
             <div className="container container-primary-px bg-fifth ">
@@ -38,7 +45,10 @@ const Index:NextPage = ()=>{
                       <span className="text-primary ml-[.7rem] text-[1.1rem]">( 1 costumer review )</span>
                     </div>
                     <div className="text-fourth">
-                            <span className="text-[1.9rem] font-bold mr-[.2rem]">
+                        <span className="text-[1.8rem] font-inter mr-[.3rem] line-through opacity-40">
+                          $99.00
+                        </span>
+                            <span className="text-[1.8rem] font-semibold font-inter mr-[.3rem]">
                             $59.00 
                             </span>
                             <span className="capitalize  text-[1.1rem]">
@@ -51,7 +61,7 @@ const Index:NextPage = ()=>{
                     <div className="flex  py-[.8rem] gap-[1.5rem] sm:flex-col">
                         <div className="border-[1px]  flex justify-between w-[120px] self-start sm:self-center ">
                             <button className="w-full text-primary border-r-[1px]  py-[.3rem]">-</button>
-                            <input type="number" name={`number of ${name}`}  className="w-full focus:outline-0 text-center text-primary"/>
+                            <input type="number" name={`number of ${name}`}  className="w-full focus:outline-0 text-center text-primary" value={0}/>
                             <button className="w-full text-primary border-l-[1px]">+</button>
                         </div>
                         <button className="bg-primary py-[.5em] px-[1.2em] text-center uppercase font-bold text-[1.1rem] text-fifth tracking-[.1rem] transition-all duration-[.3s] ease-in-out hover:bg-third focus:bg-third">
@@ -87,6 +97,24 @@ const Index:NextPage = ()=>{
                         </div>
                     </div>
                 </div>
+             </article>
+             <article>
+                <hr className="before:"/>
+                <ul className="flex gap-[1.4rem]  ">
+                    <li className={"capitalize text-third text-[1.1rem] font-bold py-[.7rem] cursor-pointer border-t-[3px]" + `${sectionType==="description"?"  border-primary ":""}`} onClick={handleChangeSection("description")}>
+                    description 
+                    </li>
+                    <li className={"capitalize text-third text-[1.1rem] font-bold py-[.7rem]  cursor-pointer border-t-[3px]" + `${sectionType==="reviews" ? "  border-primary" : ""}`} onClick={handleChangeSection("reviews")}>
+                    reviews (1)
+                    </li>
+                </ul>
+                <p className="text-fourth text-[1.2rem] leading-8 pt-[1.1rem]">
+                Auctor eros suspendisse tellus venenatis sodales purus non pellentesque amet, nunc sit eu, enim fringilla egestas pulvinar odio feugiat consectetur egestas magna pharetra cursus risus, lectus enim eget eu et lobortis faucibus.
+
+Eget odio justo ut scelerisque purus non aliquam adipiscing amet condimentum ligula diam erat sodales pharetra accumsan pellentesque at sem at eget ac hendrerit odio enim felis sit augue lorem egestas dictum vestibulum a etiam nisi, elit augue volutpat porta scelerisque nullam at leo faucibus cursus metus.
+
+Viverra nunc iaculis id sed diam nam quam id sapien pellentesque quam sed eu augue id ac tempus aliquam facilisis vivamus eget nisi id.
+                </p>
              </article>
             </div>
          </section>
