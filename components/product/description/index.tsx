67c9ff9b3467764509a5bb86be8e1f69e5dd198f
karam <karamkaku2000@gmail.com>
2022-10-23 00:48:11 -0400

    chore: add key to a list

--- components/product/description/index.tsx
+++ components/product/description/index.tsx
@@ -2,7 +2,7 @@
 interface propsI{
   description : string;
 }
-const Description = ({}:propsI)=>{
+const Description = ()=>{
   return(
     <p className="text-fourth text-[1.2rem] leading-8 pt-[1.1rem]">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, dolore maiores aperiam dolores rem voluptatem quibusdam voluptate, non ab necessitatibus voluptatum? Distinctio neque hic exercitationem et aliquid eum possimus sint.
